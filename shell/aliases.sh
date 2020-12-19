01de4f0ab883fc214749e65bb9d149f4f9510f85
Sergi Orra <sergiorra17@gmail.com>
2020-12-19 17:28:03 +0100

    alias removed

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -22,7 +22,6 @@ alias gb="git branch"
 alias gl="$DOTLY_PATH/bin/dot git pretty-log"
 
 # Utils
-alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
